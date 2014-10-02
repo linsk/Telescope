3bc887fa3f2fd58a133d96dd806f43463dbc6546
Jason Paryani <github@jparyani.com>
2014-10-02 00:40:16 -0700

    Change voting on posts to allow downvoting and weight votes

--- client/views/posts/modules/post_upvote.js
+++ client/views/posts/modules/post_upvote.js
@@ -1,7 +1,7 @@
 Template[getTemplate('postUpvote')].helpers({
   upvoted: function(){
     var user = Meteor.user();
-    if(!user) return false; 
+    if(!user) return false;
     return _.include(this.upvoters, user._id);
   },
   oneBasedRank: function(){
@@ -11,15 +11,22 @@ Template[getTemplate('postUpvote')].helpers({
 });
 
 Template[getTemplate('postUpvote')].events({
-  'click .upvote-link': function(e, instance){
+  'click .upvote': function(e, instance){
     var post = this;
     e.preventDefault();
     if(!Meteor.user()){
       Router.go(getSigninUrl());
       throwError(i18n.t("Please log in first"));
     }
-    Meteor.call('upvotePost', post, function(error, result){
-      trackEvent("post upvoted", {'_id': post._id});
-    });
+
+    if (_.include(this.upvoters, Meteor.user()._id)) {
+      Meteor.call('cancelUpvotePost', post, function(error, result){
+        trackEvent("post cancelled upvote", {'_id': post._id});
+      });
+    } else {
+      Meteor.call('upvotePost', post, function(error, result){
+        trackEvent("post upvoted", {'_id': post._id});
+      });
+    }
   }
 });
