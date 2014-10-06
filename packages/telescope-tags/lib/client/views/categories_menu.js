adc5262a09eb17c67c4a64ab8f83da78536e916d
Jason Paryani <github@jparyani.com>
2014-10-06 14:50:09 -0700

    Split tags into "Status" and "Genre"

--- packages/telescope-tags/lib/client/views/categories_menu.js
+++ packages/telescope-tags/lib/client/views/categories_menu.js
@@ -1,10 +1,16 @@
 Meteor.startup(function () {
   Template[getTemplate('categoriesMenu')].helpers({
     hasCategories: function(){
-      return typeof Categories !== 'undefined' && Categories.find().count();
+      return typeof Categories !== 'undefined' && Categories.find({name: {$nin: ['In-Progress', 'Updating', 'Ported']}}).count();
     },
     categories: function(){
-      return Categories.find({}, {sort: {order: 1, name: 1}});
+      return Categories.find({name: {$nin: ['In-Progress', 'Updating', 'Ported']}}, {sort: {order: 1, name: 1}});
+    },
+    hasStatuses: function(){
+      return typeof Categories !== 'undefined' && Categories.find({name: {$in: ['In-Progress', 'Updating', 'Ported']}}).count();
+    },
+    statuses: function(){
+      return Categories.find({name: {$in: ['In-Progress', 'Updating', 'Ported']}}, {sort: {order: 1, name: 1}});
     },
     categoryLink: function () {
       return getCategoryUrl(this.slug);
