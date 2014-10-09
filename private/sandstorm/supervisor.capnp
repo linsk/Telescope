999a3b044d630b77a9054a11f3d3a6583e0fac1b
Jason Paryani <github@jparyani.com>
2014-10-09 15:33:30 -0700

    Add emailing of sandstorm invites

---
+++ private/sandstorm/supervisor.capnp
@@ -0,0 +1,44 @@
+# Sandstorm - Personal Cloud Sandbox
+# Copyright (c) 2014 Sandstorm Development Group, Inc. and contributors
+# All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+@0xc7205d6d32c7b040;
+
+$import "/capnp/c++.capnp".namespace("sandstorm");
+
+using Util = import "util.capnp";
+using Grain = import "grain.capnp";
+
+interface Supervisor {
+  # Default capability exported by the supervisor process.
+
+  getMainView @0 () -> (view :Grain.UiView);
+  # Get the grain's main UiView.
+
+  keepAlive @1 ();
+  # Must call periodically to prevent supervisor from killing itself off.  Call at least once
+  # per minute.
+
+  shutdown @2 ();
+  # Shut down the grain immediately.  Useful e.g. when upgrading to a newer app version.  This
+  # call will never return successfully because the process kills itself.
+
+  getGrainSize @3 () -> (size :UInt64);
+  # Get the total storage size of the grain.
+
+  getGrainSizeWhenDifferent @4 (oldSize :UInt64) -> (size :UInt64);
+  # Wait until the storage size of the grain is different from `oldSize` and then return the new
+  # size. May occasionally return prematurely, with `size` equal to `oldSize`.
+}
--- server/votingInvitations.js
+++ server/votingInvitations.js
@@ -1,6 +1,26 @@
 var csv = Meteor.npmRequire('csv');
 var csv_parse = Meteor.wrapAsync(csv.parse);
 
+var emailTemplates = Meteor.npmRequire('email-templates');
+var path = Npm.require("path");
+var templatesDir = path.resolve("./assets/app", "templates");
+var emailTemplatesSync = Meteor.wrapAsync(emailTemplates);
+
+var Future = Npm.require("fibers/future");
+var sandstormDir = path.resolve("./assets/app", "sandstorm");
+var capnp = Meteor.npmRequire('capnp');
+var HackSession = capnp.import(path.resolve(sandstormDir, "hack-session.capnp"));
+
+var promiseToFuture = function(promise) {
+  var result = new Future();
+  promise.then(result.return.bind(result), result.throw.bind(result));
+  return result;
+}
+
+function waitPromise(promise) {
+  return promiseToFuture(promise).wait();
+}
+
 Meteor.methods({
   uploadInvitationsCsv: function (data) {
     if (!isAdmin(Meteor.user()))
@@ -35,5 +55,46 @@ Meteor.methods({
       isInvited: true
     }});
     VotingInvites.update({_id: id}, {$set: {accepted: true}});
+  },
+
+  sendInviteEmails: function (grainUrl) {
+    if (!isAdmin(Meteor.user()))
+      throw new Meteor.Error(403, "Not an admin");
+
+    var template = emailTemplatesSync(templatesDir);
+    var templateSync = Meteor.wrapAsync(template);
+    var Render, csv_input, parsed_smtp, pass, send_mail, transportBatch, user;
+
+    var connection = capnp.connect("unix:/tmp/sandstorm-api");
+    var session = connection.restore("HackSessionContext", HackSession.HackSessionContext);
+    var publicId = waitPromise(session.getPublicId());
+    Render = function(locals) {
+      this.locals = locals;
+      this.send = function(err, html, text) {
+        if (err) {
+          console.log(err);
+        } else {
+          // TODO(someday): make this not wait on each promise and instead collect them in a list
+          waitPromise(session.send({
+            from: {name: "Sandstorm Voting App", address: publicId.publicId + "@" + publicId.hostname},
+            to: [{address: locals.invitedUserEmail}],
+            subject: "Sandstorm App Committee - Voting App Invite",
+            html: html,
+            text: text,
+            date: (new Date().getTime()) * 1000000
+          }));
+        }
+      };
+      this.batch = function(batch) {
+        batch(this.locals, templatesDir, this.send);
+      };
+    };
+    var batch = templateSync("invite", true);
+    VotingInvites.find().forEach(function (record) {
+      record.grainUrl = grainUrl;
+      record.inviteLink = grainUrl + '/voteInvitation/' + record._id;
+      var render = new Render(record);
+      render.batch(batch);
+    })
   }
 });
